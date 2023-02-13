a4b586b8ce0e1c76ca04f6b10d68ee1e1f13abcb
abg74123 <bisza_i.d@hotmail.com>
2023-02-13 14:22:33 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,14 +1,24 @@
+const line = require('@line/bot-sdk')
 const express = require('express')
 const serverless = require("serverless-http")
 const app = new express()
 const router = express.Router()
+const dotenv = require('dotenv')
+const env = dotenv.config().parsed
 
-router.get('/webhook/line',(req,res)=>{
+const lineConfig = {
+   channelAccessToken: env.ACCESS_TOKEN,
+   channelSecret:env.SECRET_TOKEN
+}
+
+router.post('/webhook',line.middleware(lineConfig),(req,res)=>{
    console.log("### START WEBHOOK LINE ###");
    console.log("webhook => ",req);
-   res.json({
-      message:'Webhook Event Success'
-   })
+   const events = req.body.events
+   console.log("event =>>>>",events)
+   // res.json({
+   //    message:'Webhook Event Success'
+   // })
 })
 
 // https://thunderous-dodol-b30b53.netlify.app/.netlify/functions/api
