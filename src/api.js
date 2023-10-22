faef1dac3f12039507ff5773e6b0785fa3480bdc
kranchit precha <bisza_i.d@hotmail.com>
2023-10-22 20:03:44 +0700

    update

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const serverless = require("serverless-http")
 const app = new express()
 const router = express.Router()
 
-const lineDomain = "https://api.line.me/oauth2/v2"
+const lineDomain = "https://api.line.me/oauth2/v2.1"
 
 app.use(function (req, res, next) {
 
