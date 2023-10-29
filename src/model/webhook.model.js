250c116f3726440521b173d1fd3561ff324b55c3
kranchit precha <bisza_i.d@hotmail.com>
2023-10-29 22:28:11 +0700

    update

--- src/model/webhook.model.js
+++ src/model/webhook.model.js
@@ -1,14 +1,16 @@
-const {Client} = require('@line/bot-sdk')
+const line = require('@line/bot-sdk')
 
 export async function webhook(item) {
     console.log("--- ---MODEL | START FUNCTION webhook---")
     console.log("--- ---item---", item)
     try {
-
+        const client = new line.Client({
+            channelAccessToken: "xGCCFP74flkBjADem0hz6VoxoY/4bSGIlFfI9jzctGsVDMr8Fn1izevDyHT6pkTYEjPb/nR2ZpY8SgSTLx4HkjMJLHq+QWd0Ri6Ub+ry30yjTdw6jwYZ0Nv50vEQAijMpZJBAjzmQCQnw5cEiQR1jQdB04t89/1O/w1cDnyilFU="
+        });
         if (item.message.type === 'text') {
             switch (item.message.text) {
                 case 'หนี้สินทั้งหมด':
-                    Client.replyMessage(item.replyToken, 'จวย', true)
+                    client.replyMessage(item.replyToken, 'จวย')
                     break;
                 default:
                     break;
