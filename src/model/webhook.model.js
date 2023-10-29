9ddf074d2edb5242e5f2c2a17ec85122370ed586
kranchit precha <bisza_i.d@hotmail.com>
2023-10-29 21:50:00 +0700

    update

--- src/model/webhook.model.js
+++ src/model/webhook.model.js
@@ -1,4 +1,18 @@
+const {Client} = require('@line/bot-sdk')
+
 export async function webhook(event){
     console.log("---MODEL | START FUNCTION webhook---")
     console.log("---event---",event)
+
+    if(event.message.type === 'text'){
+        switch (event.message.text) {
+            case 'หนี้สินทั้งหมด':
+                Client.replyMessage(event.replyToken,'จวย')
+                break;
+        
+            default:
+                break;
+        }
+
+    }
 }
